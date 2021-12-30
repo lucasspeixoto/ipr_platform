05d50d93f194497a2cc4a60cc0910055ce3919a3
Lucas Peixoto <lspeixoto@stefanini.com>
2021-12-29 23:05:23 -0300

    Remoção de imports não utilizados

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -9,19 +9,15 @@ import Footer from '@components/layout/Footer';
 import { useMembers } from '@hooks/useMembers';
 import { SuspenseLoader } from '@components/pages/SuspenseLoader/index';
 import { ProfileData } from './ProfileData';
-import { useAuth } from '@hooks/useAuth';
 
 export const UserProfile: React.FC = () => {
   const { activeMember } = useMembers();
-  const { user } = useAuth();
 
   const [step, setStep] = useState(1);
   const [routeForNextStep, setRouteForNextStep] = useState<string>('/');
 
   useEffect(() => {
     if (activeMember) {
-      const { personal, supplementary, ecclesiastical } = activeMember;
-
       if (activeMember?.process) {
         if (activeMember?.process?.hasEcclesiastical) {
           setStep(4); // go to '/registration/resume'
@@ -40,24 +36,9 @@ export const UserProfile: React.FC = () => {
         setStep(1); // go to '/registration/personal'
         setRouteForNextStep('/registration/personal');
       }
-
-      /* if (ecclesiastical) {
-        setStep(4); // go to '/registration/resume'
-        setRouteForNextStep('/registration/resume');
-      } else if (supplementary) {
-        setStep(3); // go to '/registration/ecclesiastical'
-        setRouteForNextStep('/registration/ecclesiastical');
-      } else if (personal) {
-        setStep(2); // go to '/registration/supplementary'
-        setRouteForNextStep('/registration/supplementary');
-      } else {
-        setStep(1); // go to '/registration/personal'
-        setRouteForNextStep('/registration/personal');
-      } */
     }
   }, [activeMember]);
 
- 
   return (
     <>
       {activeMember ? (
